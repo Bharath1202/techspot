3979a37daa587433c380de49d06ff163b8e1f8a4
Bharath <guganathan@upturntechnology.com>
2022-10-30 12:06:12 +0530

    new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: "http://localhost:3003/api",
+  imageUrl: "https://upturnhrmanagement.s3.ap-south-1.amazonaws.com/employee/men.png",
+
 };
